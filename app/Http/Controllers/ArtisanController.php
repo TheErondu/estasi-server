482226e6c4edaaf8c6175a07a52b2650daa736fa
Erondu <erone007@gmail.com>
2023-03-25 10:41:13 +0100

    changed readme

--- app/Http/Controllers/ArtisanController.php
+++ app/Http/Controllers/ArtisanController.php
@@ -2,25 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 
 class ArtisanController extends ApiController
 {
-    public function index()
 
-    {
-        return view('artisan.index');
-    }
-
-    public function runCommand(Request $request)
-    {
-        $command = $request->input('command');
-        Artisan::call("$command");
-        $output = Artisan::output();
-        return back()->withMessage($output);;
-    }
-    public function run(Request $request)
+    public function run(Request $request): JsonResponse
     {
 
         // Call and Artisan command from within your application.
